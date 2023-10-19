c6fe43843a2017753e1ce40a34687e87444ba222
Matt <director@acidgambit.com>
2023-10-19 07:28:02 -0400

    add clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 import { headers } from "next/headers";
 import { Analytics } from "@vercel/analytics/react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -24,12 +25,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider headers={headers()}>
-          {children}
-          <Analytics />
-        </TRPCReactProvider>
-      </body>
+      {/* <ClerkProvider
+        appearance={{
+          variables: { colorPrimary: "#000000" },
+          elements: {
+            formButtonPrimary:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            socialButtonsBlockButton:
+              "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+            socialButtonsBlockButtonText: "font-semibold",
+            formButtonReset:
+              "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+            membersPageInviteButton:
+              "bg-black border border-black border-solid hover:bg-white hover:text-black",
+            card: "bg-[#fafafa]",
+          },
+        }}
+      > */}
+      <ClerkProvider>
+        <body className={`font-sans ${inter.variable}`}>
+          <TRPCReactProvider headers={headers()}>
+            <main className="flex min-h-screen flex-col items-center justify-center bg-zinc-900 text-white">
+              {children}
+            </main>
+            <Analytics />
+          </TRPCReactProvider>
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
