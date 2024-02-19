6d497da4b7655539762bd1fc8959be5595b1f1d0
Matt <director@acidgambit.com>
2024-02-19 07:31:31 -0500

    rm time lock for submitting program wods

--- src/app/(home)/home/insert/page.tsx
+++ src/app/(home)/home/insert/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="container flex flex-col items-center justify-center gap-6 px-4 py-6">
       <h1 className="text-2xl font-bold">Insert Page</h1>
-      <BulkInsert />
+      {/* <BulkInsert /> */}
     </div>
   );
 }
