086680e7459bcc371bdce0a3de9ea21cd6ff4f03
Matt <director@acidgambit.com>
2024-05-21 13:34:02 -0400

    stop using mjs

--- src/lib/stripe/config.ts
+++ src/lib/stripe/config.ts
@@ -2,10 +2,13 @@ import Stripe from "stripe"
 
 import { env } from "@/env.mjs"
 
-// // stripe config
-// export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-//   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//   // @ts-ignore
-//   apiVersion: "2022-11-15",
-//   typescript: true,
-// })
+// Ensure defined
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is not defined");
+}
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  apiVersion: "2022-11-15",
+  typescript: true,
+})
