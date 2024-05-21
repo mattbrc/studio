e41bc80af26281911267ea07eb537734dde5a18a
Matt <director@acidgambit.com>
2024-05-21 13:16:17 -0400

    stripe eslint disable

--- src/lib/stripe/config.ts
+++ src/lib/stripe/config.ts
@@ -1,10 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import Stripe from "stripe"
 
 import { env } from "@/env.mjs"
 
 // stripe config
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   apiVersion: "2022-11-15",
   typescript: true,
