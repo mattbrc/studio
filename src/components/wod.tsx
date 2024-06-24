67dcb5c69f0a511a6b7286324ae697954e5e7946
Matt <director@acidgambit.com>
2024-06-24 16:59:03 -0400

    update wod page

--- src/components/wod.tsx
+++ src/components/wod.tsx
@@ -51,7 +51,7 @@ export function Wod({ data }: WorkoutProps) {
           </div>
         </CardHeader>
         <CardContent>
-          <CardDescription className="flex flex-col gap-2">
+          <div className="text-sm text-muted-foreground">
             <p>
               Daily functional workouts written by our resident CrossFit coach
               and endurance athlete, Lee. The focus is on building a functional
@@ -59,76 +59,70 @@ export function Wod({ data }: WorkoutProps) {
               nasty heart pounders.
             </p>
             <p>The general structure will look like:</p>
-            <div>
-              <ul>
-                <li>- Monday: Strength</li>
-                <li>- Tuesday: HI Intervals</li>
-                <li>- Wednesday: METCON</li>
-                <li>- Thursday: Steady State Cardio</li>
-                <li>- Friday: Strength</li>
-                <li>- Saturday: METCON</li>
-                <li>- Sunday: Rest</li>
-              </ul>
-            </div>
-          </CardDescription>
+            <ul>
+              <li>- Monday: Strength</li>
+              <li>- Tuesday: HI Intervals</li>
+              <li>- Wednesday: METCON</li>
+              <li>- Thursday: Steady State Cardio</li>
+              <li>- Friday: Strength</li>
+              <li>- Saturday: METCON</li>
+              <li>- Sunday: Rest</li>
+            </ul>
+          </div>
         </CardContent>
       </Card>
     );
 
   return (
-    <Card className="w-full md:w-1/2">
-      <CardHeader>
-        <div className="flex items-center justify-between">
-          <div>
-            <CardTitle>Daily Functional</CardTitle>
-            <CardDescription>
-              {data?.date
-                ? formatUTCDate(data.date.toUTCString())
-                : "No date available"}
-            </CardDescription>
+    <>
+      <Card className="w-full md:w-1/2">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <div>
+              <CardTitle>Daily Functional</CardTitle>
+              <CardDescription>
+                {data?.date
+                  ? formatUTCDate(data.date.toUTCString())
+                  : "No date available"}
+              </CardDescription>
+            </div>
+            <div className="flex flex-col gap-2">
+              <WodOperations workoutId={data?.wodId} />
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <WodOperations workoutId={data?.wodId} />
-          </div>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <p className="font-bold">{data?.title}</p>
-        <p className="font-bold">Strength:</p>
-        <span>
-          <ul>
-            {Object.entries(str).map(([key, value]) => (
-              <li key={key}>{`${value}`}</li>
-            ))}
-          </ul>
-        </span>
-        <p className="mt-2 font-bold">Conditioning:</p>
-        <span>
-          <ul>
-            {Object.entries(cond).map(([key, value]) => (
-              <li key={key}>{`${value}`}</li>
-            ))}
-          </ul>
-        </span>
-      </CardContent>
-    </Card>
-  );
-}
+        </CardHeader>
+        <CardContent>
+          <p className="font-bold">{data?.title}</p>
+          <p className="font-bold">Strength:</p>
+          <span>
+            <ul>
+              {Object.entries(str).map(([key, value]) => (
+                <li key={key}>{`${value}`}</li>
+              ))}
+            </ul>
+          </span>
 
-const ProgramDetails = () => {
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        {/* <Button size="sm" variant="secondary">
-          <div>
-            <span>Details</span>
+          <p className="mt-2 font-bold">Conditioning:</p>
+          <span>
+            <ul>
+              {Object.entries(cond).map(([key, value]) => (
+                <li key={key}>{`${value}`}</li>
+              ))}
+            </ul>
+          </span>
+        </CardContent>
+      </Card>
+
+      <Card className="w-full md:w-1/2">
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <div>
+              <CardTitle>About Daily Functional</CardTitle>
+            </div>
           </div>
-        </Button> */}
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Daily Functional</AlertDialogTitle>
-          <AlertDialogDescription className="flex flex-col gap-2">
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-2 text-sm text-muted-foreground">
             <p>
               Daily functional workouts written by our resident CrossFit coach
               and endurance athlete, Lee. The focus is on building a functional
@@ -136,25 +130,36 @@ const ProgramDetails = () => {
               nasty heart pounders.
             </p>
             <p>The general structure will look like:</p>
-            <div>
-              <ul>
-                <li>- Monday: Strength</li>
-                <li>- Tuesday: HI Intervals</li>
-                <li>- Wednesday: METCON</li>
-                <li>- Thursday: Steady State Cardio</li>
-                <li>- Friday: Strength</li>
-                <li>- Saturday: METCON</li>
-                <li>- Sunday: Rest</li>
-              </ul>
-            </div>
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Close</AlertDialogCancel>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+            <ul>
+              <li>- Monday: Strength</li>
+              <li>- Tuesday: HI Intervals</li>
+              <li>- Wednesday: METCON</li>
+              <li>- Thursday: Steady State Cardio</li>
+              <li>- Friday: Strength</li>
+              <li>- Saturday: METCON</li>
+              <li>- Sunday: Rest</li>
+            </ul>
+          </div>
+        </CardContent>
+      </Card>
+    </>
   );
-};
+}
 
-export default ProgramDetails;
+// <p>
+//   Daily functional workouts written by our resident CrossFit coach
+//   and endurance athlete, Lee. The focus is on building a functional
+//   base around the core compound lifts, steady state endurance, and
+//   nasty heart pounders.
+// </p>
+// <p>The general structure will look like:</p>
+
+//   <ul>
+//     <li>- Monday: Strength</li>
+//     <li>- Tuesday: HI Intervals</li>
+//     <li>- Wednesday: METCON</li>
+//     <li>- Thursday: Steady State Cardio</li>
+//     <li>- Friday: Strength</li>
+//     <li>- Saturday: METCON</li>
+//     <li>- Sunday: Rest</li>
+//   </ul>
