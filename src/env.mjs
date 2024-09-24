321ad05575e690d6a30e21888ca439774835a85a
Matt <director@acidgambit.com>
2024-09-24 11:43:22 -0400

    mjs

--- src/env.mjs
+++ src/env.mjs
@@ -17,8 +17,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    OPENAI_API_KEY: z.string(),
-    OPENAI_ORG_ID: z.string(),
+    // OPENAI_API_KEY: z.string(),
+    // OPENAI_ORG_ID: z.string(),
   },
 
   /**
@@ -38,8 +38,8 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    OPENAI_ORG_ID: process.env.OPENAI_ORG_ID,
+    // OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    // OPENAI_ORG_ID: process.env.OPENAI_ORG_ID,
     // NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
     //   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
