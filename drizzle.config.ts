f9d4b9f9432212f06443ba4eabcaccd394e0acfc
Matt <director@acidgambit.com>
2025-04-23 07:23:44 -0700

    clean up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,3 @@
-// import { type Config } from "drizzle-kit";
-
-// import { env } from "~/env.mjs";
-
-// export default {
-//   dialect: "mysql",
-//   schema: "./src/server/db/schema.ts",
-//   driver: "mysql2",
-//   dbCredentials: {
-//     connectionString: env.DATABASE_URL,
-//   },
-//   tablesFilter: ["studio_*"],
-// } satisfies Config;
-
 import { type Config } from "drizzle-kit";
 
 import { env } from "~/env.mjs";
